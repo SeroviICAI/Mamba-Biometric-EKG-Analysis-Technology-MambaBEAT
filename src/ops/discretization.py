e52ad9d91e65f3756fc60a48012a190911c86a90
Sergio Rodríguez Vidal <202113624@alu.comillas.edu>
2024-04-17 23:41:30 +0200

    Wrong implementation of backward
    The derivative of a matrix exponential is not the product of the matrix and the exponential.

--- src/ops/discretization.py
+++ src/ops/discretization.py
@@ -28,23 +28,7 @@ class Discretize(torch.autograd.Function):
         E = torch.eye(A.size(0), dtype=A.dtype, device=A.device)
         if method == "zoh":
             # Compute gradients for ZOH method
-            Ad = torch.matrix_exp(dt * A)
-
-            grad_dt = grad_Ad @ (A * Ad)
-            grad_dt += grad_Bd.t() @ (
-                (torch.inverse(A) @ (Ad - E) + torch.inverse(dt * A) @ (A * Ad)) @ B
-                + (torch.inverse(A) @ (A * Ad)
-                + torch.inverse(A) @ ((A * Ad) + torch.inverse(dt * A) @ (A * Ad)))
-                @ (dt * B)
-            )
-
-            grad_A = grad_Ad @ (dt * Ad)
-            grad_A += grad_Bd.t() @ (
-                ((torch.inverse(dt) * E) @ (Ad - E) + torch.inverse(dt * A) @ (dt * Ad))
-                @ (dt * B)
-            )
-
-            grad_B = grad_Bd.t() @ ((torch.inverse(dt * A) @ (Ad - E)) @ grad_dt)
+            ...
 
         elif method == "bilinear":
             # Compute gradients for Bilinear method
@@ -52,7 +36,7 @@ class Discretize(torch.autograd.Function):
         else:
             raise ValueError("Invalid method. Choose either 'zoh' or 'bilinear'.")
 
-        return grad_dt, grad_A, grad_B.t(), None
+        return grad_dt, grad_A, grad_B, None
 
 
 def discretize(dt, A, B, method="zoh"):
