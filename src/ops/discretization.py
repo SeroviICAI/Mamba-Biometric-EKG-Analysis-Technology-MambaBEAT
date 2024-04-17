53b5a3faddca3fe99efaf6550df008b5065a2a35
Sergio Rodríguez Vidal <202113624@alu.comillas.edu>
2024-04-17 12:33:30 +0200

    Update *non-working* discretization.py
    Attempted to calculate backward of ZOH discretization. Dimensions match, but results don't.

--- src/ops/discretization.py
+++ src/ops/discretization.py
@@ -11,12 +11,10 @@ class Discretize(torch.autograd.Function):
         if method == "zoh":
             # Zero-Order Hold (ZOH) method
             Ad = torch.matrix_exp(dt * A)
-            Bd = torch.inverse(dt * A) @ (Ad - E) @ dt * B
+            Bd = torch.inverse(dt * A) @ (Ad - E) @ (dt * B)
         elif method == "bilinear":
             # Bilinear Method (Tustin’s method)
-            half_dt_A = 0.5 * dt * A
-            Ad = torch.inverse(E - half_dt_A) @ (E + half_dt_A)
-            Bd = torch.inverse(E - half_dt_A) @ dt * B
+            ...
         else:
             raise ValueError("Invalid method. Choose either 'zoh' or 'bilinear'.")
 
@@ -31,22 +29,30 @@ class Discretize(torch.autograd.Function):
         if method == "zoh":
             # Compute gradients for ZOH method
             Ad = torch.matrix_exp(dt * A)
-            Bd = torch.inverse(dt * A) @ (Ad - E) @ dt * B
-            grad_A = grad_Ad @ Ad @ dt + grad_Bd @ Bd @ dt
-            grad_B = grad_Bd @ Bd
-            grad_dt = grad_Ad @ (Ad @ A) + grad_Bd @ (Bd @ B)
+
+            grad_dt = grad_Ad @ (A * Ad)
+            grad_dt += grad_Bd.t() @ (
+                (torch.inverse(A) @ (Ad - E) + torch.inverse(dt * A) @ (A * Ad)) @ B
+                + (torch.inverse(A) @ (A * Ad)
+                + torch.inverse(A) @ ((A * Ad) + torch.inverse(dt * A) @ (A * Ad)))
+                @ (dt * B)
+            )
+
+            grad_A = grad_Ad @ (dt * Ad)
+            grad_A += grad_Bd.t() @ (
+                ((torch.inverse(dt) * E) @ (Ad - E) + torch.inverse(dt * A) @ (dt * Ad))
+                @ (dt * B)
+            )
+
+            grad_B = grad_Bd.t() @ ((torch.inverse(dt * A) @ (Ad - E)) @ grad_dt)
+
         elif method == "bilinear":
             # Compute gradients for Bilinear method
-            half_dt_A = 0.5 * dt * A
-            Ad = torch.inverse(E - half_dt_A) @ (E + half_dt_A)
-            Bd = torch.inverse(E - half_dt_A) @ dt * B
-            grad_A = 0.5 * dt * (grad_Ad @ (Ad @ A) - grad_Ad @ Ad + grad_Bd @ Bd)
-            grad_B = grad_Bd @ Bd
-            grad_dt = 0.5 * (grad_Ad @ (Ad @ A) - grad_Ad @ Ad + grad_Bd @ (Bd @ B))
+            ...
         else:
             raise ValueError("Invalid method. Choose either 'zoh' or 'bilinear'.")
 
-        return grad_dt, grad_A, grad_B
+        return grad_dt, grad_A, grad_B.t(), None
 
 
 def discretize(dt, A, B, method="zoh"):
